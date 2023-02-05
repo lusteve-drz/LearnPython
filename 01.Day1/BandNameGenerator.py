19fc20823b84b0d42bf8d463dbbf41fa05626bed
Steve-Lu <105989004+lusteve-drz@users.noreply.github.com>
2023-02-05 00:19:15 -0500

    Day 1 Done!

---
+++ 01.Day1/BandNameGenerator.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+#2. Ask the user for the city that they grew up in.
+#3. Ask the user for the name of a pet.
+#4. Combine the name of their city and pet and show them their band name.
+#5. Make sure the input cursor shows on a new line.
+
+print('Welcome to Band Name Generator App!')
+city = input('Which city are you grew up? \n')
+pet = input('What is your pet name? \n')
+print(f'Your band name is: {city} {pet}')
---
+++ 01.Day1/exercises.py
@@ -0,0 +1,36 @@
+# Exe 1
+# Day 1 - Python Print Function
+# The function is declared like this:
+# print('what to print')
+
+# print('Day 1 - Python Print Function')
+# print('The function is declared like this:')
+# print("print('what to print')")
+# ----------------------------------------------------------------------------------------------------------------
+# Exe 2
+# Day 1 - String Manipulation
+# String Concatenation is done with the "+" sign.
+# e.g. print("Hello " + "world")
+# New lines can be created with a backslash and n.
+
+# print('Day 1 - String Manipulation')
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello" + "world")')
+# ----------------------------------------------------------------------------------------------------------------
+# Exe 3
+# Input function if type Steve output will calculate the string
+
+# name = input('What is your name? ')
+# print(f'Total of your name count is: {len(name)}')
+# ----------------------------------------------------------------------------------------------------------------
+# Exe 4
+# Example a value is 5 and b value is 4. Exchange the value of each variable and print out.
+
+# a = input('a : ')
+# b = input('b : ')
+# c = a
+# a = b
+# b = c
+# print(f'a : {a}')
+# print(f'b : {b}')
+# ----------------------------------------------------------------------------------------------------------------
