bb4c3c8ce809b5984bc60d81b8c2b83eca1957ba
Steve-Lu <105989004+lusteve-drz@users.noreply.github.com>
2023-02-05 01:11:47 -0500

    Day 3 In Progress
    Finished to Exe 3.

---
+++ 03.Day3/exercises.py
@@ -0,0 +1,43 @@
+# Exe 1
+# Write a program that works out whether if a given number is an odd or even number.
+# Even numbers can be divided by 2 with no remainder.
+# e.g. 86 is even because 86 ÷ 2 = 43
+# 43 does not have any decimal places. Therefore, the division is clean.
+# e.g. 59 is odd because 59 ÷ 2 = 29.5
+
+# number = int(input('Please enter a number!: '))
+# if number % 2 == 0 :
+#     print(f'{number} is a even number.')
+# else:
+#     print(f'{number} is an odd number.')
+# --------------------------------------------------------------------------------------------
+# Exe 2
+# Write a program that interprets the Body Mass Index (BMI) based on a user's weight and height.
+# It should tell them the interpretation of their BMI based on the BMI value.
+#
+# Under 18.5 they are underweight
+# Over 18.5 but below 25 they have a normal weight
+# Over 25 but below 30 they are slightly overweight
+# Over 30 but below 35 they are obese
+# Above 35 they are clinically obese.
+
+# weight = float(input('What is your weight?: '))
+# height = float(input('What is your height?: '))
+# bmi = round(weight / (height * height), 2)
+#
+# if bmi <= 18.5:
+#     print(f'Your BMI is {bmi}, you are underweight.')
+# elif bmi > 18.5 and bmi <= 25:
+#     print(f'Your BMI is {bmi}, you have a normal weight.')
+# elif bmi > 25 and bmi <= 30:
+#     print(f'Your BMI is {bmi}, you are slightly overweight.')
+# elif bmi > 30 and bmi <= 35:
+#     print(f'Your BMI is {bmi}, you are obese!')
+# else:
+#     print(f'Your BMI is {bmi}, you are clinically obese!!!')
+# --------------------------------------------------------------------------------------------
+# Exe 3
+
+
+
+
